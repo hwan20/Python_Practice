005923b10c79e9bc2ab559b32d900c99f8803bcb
hwan20 <ejrwjd20@naver.com>
2022-04-18 10:23:24 +0900

    Django연습문제 수정

--- django2_ex/myapp/views.py
+++ django2_ex/myapp/views.py
@@ -6,8 +6,9 @@ def main(request):
     return render(request, "main.html")
 
 def transFunc(request):
-    request.method == 'GET' 
-    gender = request.GET['gender']
-    return render(request, "show.html", {'gender':gender})
-
+    if request.method == 'GET': #if를 사용해야 함 request.method 는 bool type으로 T와 F를 반환 GET이면 T POST면 F
+        gender = request.GET['gender']
+        return render(request, "show.html", {'gender':gender})
+    else:
+        print('요청 에러')
 
