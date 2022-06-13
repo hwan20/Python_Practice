a87bb02409c97eb2a600ee0d1fe5d3e8fd4951a1
hwan20 <ejrwjd20@naver.com>
2022-06-13 09:45:39 +0900

    django에서 crud

--- django11_crud/django11_crud/settings.py
+++ django11_crud/django11_crud/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'test',   
         'USER': 'root',        
-        'PASSWORD': '1234', #컴터에서는 비밀번호 123
+        'PASSWORD': '123', #컴터에서는 비밀번호 123
         'HOST': '127.0.0.1',               
         'PORT': '3306',                
     }
